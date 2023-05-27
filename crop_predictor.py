15c7c1e490c8724f4efe24649018411e0da4920d
dhruvjain04 <68044412+dhruvjain04@users.noreply.github.com>
2023-05-27 21:49:34 +0530

    Add files via upload

---
+++ crop_predictor.py
@@ -0,0 +1,26 @@
+import pickle
+
+import streamlit as st
+import pandas as pd
+
+st.title("Realtime crop predictor")
+file = open('finalized_model.pkl', 'rb')
+
+loaded_model = pickle.load(open('finalized_model.pkl', 'rb'))
+
+N = st.number_input("Nitrogen Content of soil")
+P = st.number_input("Phosphorous Content of soil")
+K = st.number_input("Potassium Content of soil")
+temperature = st.number_input("Current temperature of region")
+humidity = st.number_input("Humidity level of region")
+pH = st.number_input("pH value of soil")
+rainfall = st.number_input("Amount of rainfall(in mm)")
+
+test = [[N, P, K, temperature, humidity, pH, rainfall]]
+df = pd.DataFrame(test, columns=['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall'])
+print(df)
+if st.button("Predict crop"):
+    predicted_val = loaded_model.predict(df)
+    for val in predicted_val:
+        st.success(val)
+
